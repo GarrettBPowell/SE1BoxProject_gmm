733bdc6c79680d1d697c43f29acf7ed9d075db8c
GarrettBPowell <garettbpowell@gmail.com>
2021-02-11 11:51:43 -0600

    Initial commit

---
+++ src/main/java/SE1BoxProject_gmm.java
@@ -0,0 +1,11 @@
+/**
+ * SE1 Laser Box Project
+ * Marlon Miller Matute, Garrett Powell, Michael Thorson
+ */
+public class SE1BoxProject_gmm
+{
+    public static void main( String[] args )
+    {
+        System.out.println("<path d=\"M 45.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />");
+    }
+}
---
+++ src/test/java/SE1BoxProject_gmmTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+
+/**
+*
+*
+**/
+
+public class SE1BoxProject_gmmTest
+{
+     SE1BoxProject_gmm box;
+    
+    @Before
+    public void initialize() {
+	box = new SE1BoxProject_gmm();
+    }
+    
+    //test add func 1
+    @Test
+    public void helloBox() 
+    {
+		//testAdd1
+    	try {
+
+	    assert(true);
+		} catch (Exception e) {
+			System.out.println("Hello Box Error");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
