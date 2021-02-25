dd606f3157a64624c9d8ea4373beadb11074a9d9
GarrettBPowell <garettbpowell@gmail.com>
2021-02-25 13:12:06 -0600

    Added create file

--- src/main/java/SE1BoxProject_gmm.java
+++ src/main/java/SE1BoxProject_gmm.java
@@ -2,10 +2,16 @@
  * SE1 Laser Box Project
  * Marlon Miller Matute, Garrett Powell, Michael Thorson
  */
+ 
+import java.io.File;  // Import the File class
+import java.io.IOException;
+
+
 public class SE1BoxProject_gmm
 {
     public static void main( String[] args )
     {
+		createFile("Test");
         System.out.println("<path d=\"M 45.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />");
     }
 
@@ -20,4 +26,25 @@ public class SE1BoxProject_gmm
 
 	    return false;
     }
+	
+	public static void createFile(String fileName)
+	{
+		 try 
+		 {
+			File myObj = new File(fileName + ".svg");
+			  
+			if (myObj.createNewFile()) {
+				System.out.println("File created: " + myObj.getName());
+			}
+			else 
+			{
+				System.out.println("File already exists.");
+			}
+		} 
+		catch (IOException e) 
+		{
+		  System.out.println("An error occurred.");
+		  e.printStackTrace();
+		}
+	}
 }
