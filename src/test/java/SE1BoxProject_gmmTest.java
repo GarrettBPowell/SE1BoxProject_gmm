87f4bd4a64fd09e702d3699842d110ac5c4f0f7f
GarrettBPowell <garettbpowell@gmail.com>
2021-02-25 13:33:01 -0600

    added test case for create file

--- src/test/java/SE1BoxProject_gmmTest.java
+++ src/test/java/SE1BoxProject_gmmTest.java
@@ -3,6 +3,10 @@ import static org.junit.Assert.assertTrue;
 import org.junit.*;
 
 /**
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+
+/**
 *
 *
 **/
@@ -28,14 +32,43 @@ public class SE1BoxProject_gmmTest
 			System.out.println("Hello Box Error");
 		}
 	}
+
+    @Test 
+    public void correctString()
+    {
+        try {
+            assertEquals(true,box.coordinates("<path d=\"M 35.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />"));
+            assertEquals(true,box.coordinates("<path d=\"M 45.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />"));
+            assertEquals(true,box.coordinates("<path d=\"M 55.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />"));
+
+            assertEquals(false,box.coordinates("<path d=\"M 55.0 55.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />"));
+
+            assertEquals(true,box.coordinates("<path d=\"M 45.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />"));
+            assertEquals(true,box.coordinates("<path d=\"M 35.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />"));
+            assertEquals(true,box.coordinates("<path d=\"M 55.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />"));
+
+
+        }
+        catch (Exception e) {
+            System.out.println("Wrong coordinates");
+        }
+
+    }
+	
+	@Test
+	public void fileCreation()
+	{
+		try{
+			int randA = (int)(Math.random() * 10000);
+			int randB = (int)(Math.random() * 10000);
+			
+			assertTrue(box.createFile("Test File " + randA + " " + randB));
+		}
+		catch (Exception e) {
+			System.out.println("Failed to create file, file may already exist, please delete file, or move file");
+		}
+	}
 }
-
-
-
-
-
-
-
 
 
 
