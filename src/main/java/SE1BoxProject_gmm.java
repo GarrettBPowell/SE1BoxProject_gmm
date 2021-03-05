77e8e8ebc3b6ef275362f55e5db206ca977de039
Marlon Miller <74426729+mmiller2001@users.noreply.github.com>
2021-03-05 01:23:46 -0600

    Update SE1BoxProject_gmm.java

--- src/main/java/SE1BoxProject_gmm.java
+++ src/main/java/SE1BoxProject_gmm.java
@@ -10,68 +10,110 @@ import java.io.*;
 
 public class SE1BoxProject_gmm
 {
+    public static String width;
+    public static String length;
+    public static String height;
+    public static String fileName;
+
     public static void main( String[] args )
     {
         Scanner sc = new Scanner(System.in);
         System.out.print("Welcome to your Box Creator!\nPlease enter the following measurements for the box.\n");
 
+        System.out.print("\nEnter your SVG Filename: ");
+        fileName = sc.nextLine();
+
         System.out.print("Enter the width between (0.0-100.0)cm: ");
-        String Str = sc.nextLine();
-        float width = Float.parseFloat(Str); 
+        width = sc.nextLine();
 
         System.out.print("Enter the length between (0.0-100.0)cm: ");
-        Str = sc.nextLine();
-        float length = Float.parseFloat(Str); 
+        length = sc.nextLine();
 
         System.out.print("Enter the height between (0.0-100.0)cm: ");
-        Str = sc.nextLine();
-        float height = Float.parseFloat(Str); 
+        height = sc.nextLine();
 
-        System.out.print("You have entered: "+ (width+1) + "\n");
-        System.out.print("You have entered: "+ (length+1) + "\n");
-        System.out.print("You have entered: "+ (height+1) + "\n");
+        createFile(fileName);
     }
     
     public boolean coordinates(String keys)
     {
-    	String corners[] = {"<path d=\"M 35.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 45.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 55.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 45.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 35.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 55.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />"};
-    	int length = corners.length;
+        String corners[] = {"<path d=\"M 35.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 45.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 55.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 45.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 35.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />","<path d=\"M 55.0 45.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />"};
+        int length = corners.length;
 
-	    for(int i = 0; i < length; i++)
-	    	if(keys == corners[i])
-	    		return true;
+        for(int i = 0; i < length; i++)
+            if(keys == corners[i])
+                return true;
 
-	    return false;
+        return false;
     }
-	
-	public static boolean createFile(String fileName)
-	{
-		try 
-		{
-			File newFile = new File(fileName + ".svg");
-			
+    
+    public static boolean createFile(String fileName)
+    {
+        try 
+        {
+            File newFile = new File(fileName + ".svg");
+            
      
-			if (newFile.createNewFile()) {
-				System.out.println("File created: " + newFile.getName() + " in project folder");
+            if (newFile.createNewFile()) {
+                System.out.println("File created: " + newFile.getName() + " in project folder\n");
 
-					BufferedWriter toFile = new BufferedWriter(new FileWriter(fileName + ".svg"));
-					toFile.write("<?xml version='1.0' encoding='us-ascii'?>");
-					toFile.write("\n<svg height=\"81.90mm\" viewBox=\"0.0 0.0 120.10 81.90\" width=\"120.10mm\" xmlns\"http://www.w3.org/2000/svg\" xmlns:cc=\"http://creativecommons.org/ns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
-					toFile.close();
-				return true;
-			}
-			else 
-			{
-				System.out.println("Failed to create file, file may already exist, please delete file, or move file");
-				return false;
-			}
-		} 
-		catch (IOException e) 
-		{
-		  System.out.println("An error occurred.");
-		  e.printStackTrace();
-		}
-		return false;
-	}
+                    BufferedWriter toFile = new BufferedWriter(new FileWriter(fileName + ".svg"));
+                    toFile.write("<?xml version='1.0' encoding='us-ascii'?>");
+                    toFile.write("\n<svg height=\"81.90mm\" viewBox=\"0.0 0.0 120.10 81.90\" width=\"120.10mm\" xmlns\"http://www.w3.org/2000/svg\" xmlns:cc=\"http://creativecommons.org/ns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
+                    toFile.write("\n<g id=\"dovetail\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">");
 
+                    String newPath = pathCreation(width,height);
+                    System.out.println("New path created as: "+newPath);
+                    //toFile.write("\n"+newPath);
+
+                    toFile.write("\n</g>");
+                    toFile.write("\n</svg>");
+                    toFile.close();
+                return true;
+            }
+            else 
+            {
+                System.out.println("Failed to create file, file may already exist, please delete file, or move file");
+                return false;
+            }
+        } 
+        catch (IOException e) 
+        {
+          System.out.println("An error occurred.");
+          e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static String pathCreation(String width, String height)
+    {
+        // v -> (+)up or (-)down
+        // h -> (-)left or (+)right
+        // width and height
+        String example = "<path d=\"M 35.0 35.0 v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />";
+        String svg = "<path d=\"M 35.0 35.0"; // v -9.0 h -9.0 v 9.0 h 9.0\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />";
+
+        for(int i = 0; i < 4; i++)
+        { 
+            svg += " ";
+            if(i % 2 == 0) {
+                svg += "v ";
+                if(i < 2) 
+                    svg += "-" + height;
+                else
+                    svg += height;
+            }
+
+            else {
+                svg += "h ";
+                if(i < 2) 
+                    svg += "-" + width;
+                else
+                    svg += width;
+            }
+        }
+
+        svg += "\" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />";
+        return svg;
+    }
 }
